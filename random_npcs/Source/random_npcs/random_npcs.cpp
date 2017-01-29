78d25a9a1d0b6de6bdc5f4ffa4373139fa3c77f5
Angela <angelapennwu@gmail.com>
2017-01-29 15:55:00 -0500

    Initial commit

---
+++ random_npcs/Source/random_npcs/random_npcs.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "random_npcs.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, random_npcs, "random_npcs" );
